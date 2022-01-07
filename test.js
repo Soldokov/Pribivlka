018c162687a5b4635a4b4804ce66a50da592054b
Andrey Soldokov <87080884+Soldokov@users.noreply.github.com>
2022-01-07 19:48:44 +0300

    Add files via upload

--- test.js
+++ test.js
@@ -1,20 +1,42 @@
 let sendtram = document.getElementById("sendtram"); //кнопка отправить трамвай
 let listtram = document.getElementById("listoftram");//список трамваев
-const numbertram  = document.querySelectorAll('.text')[0];//поле текста где мы вводим номер трамвая
-const timetram  = document.querySelectorAll('.text')[1];//поле текста где мы вводим время отправления
+let final = document.querySelectorAll('.text')[0];//конечная
+let stop1  = document.querySelectorAll('.text')[1];//первая
+let stop2 = document.querySelectorAll('.text')[2];//вторая
+const NUMBERTRAM  = document.querySelectorAll('.text')[3];//поле текста где мы вводим номер трамвая
+const TIMETRAM  = document.querySelectorAll('.text')[4];//поле текста где мы вводим время отправления
+let listonstop =  document.getElementById("thisstop");
+
+let listofnumtrain = []
+let listoftimetrain = []
+
 
 sendtram.addEventListener("click", addtram); //исполняем функцию по нажатию на кнопку
 
 function addtram() 
 {
-    if (numbertram.value == "" || timetram.value == "")
+    if (NUMBERTRAM.value == "" || TIMETRAM.value == "")
     {
         alert("Неправильно введены данные или пустое поле"); //проверяем не пустое ли поле у номера трамвая или времени (про неправильное придумал, но написать его не смогу, ибо ндолго и нудно вводить проверку формы написания данных)
         return;
     }
+    listofnumtrain.push(NUMBERTRAM.value);
+    listoftimetrain.push( TIMETRAM.value);
+
+    //таблица для остановок 
+    
+
+    //таблица трамваев общая
     let newtrampoint = document.createElement("li");//создаем пункт списка 
-    newtrampoint.textContent = numbertram.value + " — " + timetram.value; //формируем данные которые поместим в пункт списка т.е. номер трамвая + время отбытия 
+    newtrampoint.textContent = NUMBERTRAM.value + " — " + TIMETRAM.value; //формируем данные которые поместим в пункт списка т.е. номер трамвая + время отбытия 
     listtram.appendChild(newtrampoint);//добавляем пункт в список 
-    numbertram.value = "";//чистим наши поля
-    timetram.value = "";//чистим наши поля 
+    NUMBERTRAM.value = "";//чистим наши поля
+    TIMETRAM.value = "";//чистим наши поля 
+}
+
+
+function viewtramonthis() 
+{
+    listonstop.classList.toggle("hidden");
+    
 }
